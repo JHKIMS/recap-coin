cdb55f1f9e45d8ec3b29fe6589208cfb5adf8f7d
JHKIMS <wjdgns5131@gmail.com>
2023-07-22 12:23:15 +0900

    Recap-Coin 홈화면 만들기 완료(ft.API)

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,86 @@
-function Coins() {
-    return <h1>Coins</h1>;
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  margin-bottom: 10px;
+  border-radius: 15px;
+  color: ${(props) => props.theme.bgColor};
+  a {
+    transition: color 0.2s ease-in;
+    display: block;
+    padding: 20px;
+    /* 이 부분에 padding값+block속성을 줘서 한줄을 <Link>태그로 클릭할 수 있게 만들어줌 */
   }
-  export default Coins;
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.span`
+    display: block;
+    text-align: center;
+`
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const[coins, setCoins] = useState<CoinInterface[]>([]);
+  const[loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async() =>{
+        const response = await fetch("https://api.coinpaprika.com/v1/coins");
+        const json = await response.json();
+        setCoins(json.slice(0,10));
+        setLoading(false);
+    })()
+  },[])
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        <Loader>"Loading..."</Loader>
+      ):(
+        <CoinList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinList>
+      )}
+      
+    </Container>
+  );
+}
+export default Coins;
